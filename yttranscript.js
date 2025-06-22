cd74ba9d38615421109e6298c4b7af5a2c891315
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2025-06-22 14:36:19 +0530

    Update yttranscript.js

--- yttranscript.js
+++ yttranscript.js
@@ -1,1 +1,2 @@
-document.querySelector("#primary-button button").click(); setTimeout(() => {     const data = [...document.querySelectorAll("ytd-transcript-segment-renderer")].map(z => z.innerText);     console.log(data) }, 6000);
+
+javascript:(function(){document.querySelector("#primary-button button").click(); setTimeout(() => {     const data = [...document.querySelectorAll("ytd-transcript-segment-renderer")].map(z => z.innerText);     console.log(data) }, 6000);})();
