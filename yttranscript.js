0c3e6c91ae050da5527a461a9808275109f712cc
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2025-06-22 14:34:05 +0530

    Create yttranscript.js

---
+++ yttranscript.js
@@ -0,0 +1,1 @@
+document.querySelector("#primary-button button").click(); setTimeout(() => {     const data = [...document.querySelectorAll("ytd-transcript-segment-renderer")].map(z => z.innerText);     console.log(data) }, 6000);
